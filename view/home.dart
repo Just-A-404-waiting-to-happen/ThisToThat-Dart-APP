a4d1f731c1786af29f283153c6da7ee37b655b40
Justin Adams <146144016+Just-A-404-waiting-to-happen@users.noreply.github.com>
2024-11-19 17:22:11 -0700

    Create home.dart

---
+++ view/home.dart
@@ -0,0 +1,69 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2850964889.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1230424322.
+import 'package:flutter/material.dart';
+import 'package:myapp/my_button.dart';
+import 'package:myapp/views/currency_screen.dart';
+import 'package:myapp/views/time_screen.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        titleTextStyle: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 255, 255, 255)),
+        title: const Text("This to That"),
+      ),
+      body: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 16),
+              MyButton(
+                text: "Base",
+                onPressed: () {}, // Do nothing for now
+                color: const Color.fromARGB(255, 81, 200, 255),
+              ),
+              const SizedBox(height: 16),
+              MyButton(
+                text: "Units",
+                onPressed: () {}, // Do nothing for now
+                color: const Color.fromARGB(255, 67, 164, 208),
+              ),
+              const SizedBox(height: 16),
+              MyButton(
+                text: "Time",
+                onPressed: () {
+                                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const TimeScreen()),
+                    );
+                }, // Do nothing for now
+                color: const Color.fromARGB(255, 53, 122, 154),
+              ),
+              const SizedBox(height: 16),
+              MyButton(
+                  text: "Currency",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const CurrencyScreen()),
+                    );
+                  },
+                  color: const Color.fromARGB(255, 96, 125, 139))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
